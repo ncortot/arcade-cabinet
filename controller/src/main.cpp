b6316af66acc57c37131d307eef4d7a5da4fa4d3
Nicolas Cortot <nc@rznc.net>
2016-06-06 12:29:17 +0200

    Convert to XINPUT controller

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,4 +1,5 @@
 #include "WProgram.h"
+#include "xinput.h"
 
 // Joystick axis positions
 #define J_MIN    0
@@ -6,28 +7,87 @@
 #define J_MAX    1023
 
 #define BUTTON_MIN 4  // First button
-#define BUTTON_MAX 24 // Last button + 1
+#define BUTTON_MAX 15 // Last button + 1
+
+
+/*
+
+    Pins mapping
+
+        3              12  13
+      0   1     8   9  10  11
+        2       4   5   6   7      14
+
+    XBox controller mapping
+
+        ^         Back  Start
+      <   >     X   Y  RB  LB
+        v       A   B  RT  LT   Guide
+
+*/
+#define PIN_LEFT    0
+#define PIN_RIGHT   1
+#define PIN_DOWN    2
+#define PIN_UP      3
+#define PIN_A       4
+#define PIN_B       5
+#define PIN_X       8
+#define PIN_Y       9
+#define PIN_RB      10
+#define PIN_LB      11
+#define PIN_RT      6
+#define PIN_LT      7
+#define PIN_BACK    12
+#define PIN_START   13
+#define PIN_GUIDE   14
+
 
 extern "C" int main(void)
 {
-    Joystick.useManualSend(true);
+    XINPUT controller(NO_LED);
+    uint8_t state[BUTTON_MAX];
 
     // Configure joystick and button pins as inputs
-    for (int i = 0; i < BUTTON_MAX; i++) {
+    for (uint8_t i = 0; i < BUTTON_MAX; i++) {
         pinMode(i, INPUT_PULLUP);
     }
 
     while (true) {
         // Read button states
         for (uint8_t i = 0; i < BUTTON_MAX; ++i) {
-            bool state = !digitalReadFast(i);
-            Joystick.button(i + 1, state);
+            state[i] = !digitalReadFast(i);
         }
 
-        // Send new state
-        Joystick.send_now();
+        // Update the DPAD
+        controller.dpadUpdate(
+            state[PIN_UP],
+            state[PIN_DOWN],
+            state[PIN_LEFT],
+            state[PIN_RIGHT]);
 
-        delay(5);
-        yield();
+        // Buttons
+        controller.buttonUpdate(BUTTON_A, state[PIN_A]);
+        controller.buttonUpdate(BUTTON_B, state[PIN_B]);
+        controller.buttonUpdate(BUTTON_X, state[PIN_X]);
+        controller.buttonUpdate(BUTTON_Y, state[PIN_Y]);
+        controller.buttonUpdate(BUTTON_RB, state[PIN_RB]);
+        controller.buttonUpdate(BUTTON_LB, state[PIN_LB]);
+        controller.buttonUpdate(BUTTON_START, state[PIN_START]);
+        controller.buttonUpdate(BUTTON_BACK, state[PIN_BACK]);
+        controller.buttonUpdate(BUTTON_LOGO, state[PIN_GUIDE]);
+
+
+        // Triggers
+        uint8_t leftTrigger = 0;
+        uint8_t rightTrigger = 0;
+        if (state[PIN_LT]) { leftTrigger = 0xFF; }
+        if (state[PIN_RT]) { rightTrigger = 0xFF; }
+        controller.triggerUpdate(leftTrigger, rightTrigger);
+
+        // Send data
+        controller.sendXinput();
+
+        // Receive data
+        controller.receiveXinput();
     }
 }
